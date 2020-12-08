907e94f2e50a67d8055bdb7266b2e81406c2b884
Bexruz Xolmuminov <bexruzxolmuminov@gmail.com>
2020-12-08 22:20:01 +0500

    small microservice post-comment

---
+++ comments/index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const { randomBytes } = require('crypto')
+const cors = require('cors')
+const axios = require('axios')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+let commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res)=>{
+  res.send(commentsByPostId[req.params.id]) || []
+})
+
+app.post('/posts/:id/comments', async (req, res)=>{
+  const commentId = randomBytes(4).toString('hex')
+  const { content } = req.body
+
+  const comments = commentsByPostId[req.params.id] || []
+
+  comments.push({ id: commentId, content })
+
+  commentsByPostId[req.params.id] = comments
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'CommentCreated',
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id
+    }
+  })
+
+  res.status(201).send(comments)
+})
+
+app.post('/events', (req, res) => {
+  console.log('Evenet Received', req.body.type)
+
+  res.send({})
+})
+
+app.listen(4001, ()=>{
+  console.log(`Server running on port 4001`)
+})
